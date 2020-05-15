0e90bafe7771a0dedd916b8a11be334acdbe0ada
colorlessenergy <brianmunozdev@gmail.com>
2020-05-15 07:03:25 -0700

    create the object and rendered the object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import uiAnimations from './ui-animations.js';
+import renderHTML from 'react-render-html';
+
 function App() {
+
+  let items = uiAnimations.map(function (item) {
+    console.log(item);
+    return (
+      <div class="codepen-container">
+        <h3>
+          {item.title}
+        </h3>
+        <p>
+          {item.description}
+        </p>
+        { renderHTML(item.codepen) }
+      </div>)
+  })
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>
-        hello this is the start
+        css wizard
       </h1>
+      <div class="codepens">
+        {items}
+      </div>
     </div>
   );
 }
